cc0920af0f88c2324c3d9b8412ab10b29719fcdd
Aram Kocharyan <akarmenia@gmail.com>
2015-12-31 12:53:15 +1100

    Version 0.2.3

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 // Meteor package definition.
 Package.describe({
   name: 'aramk:tz-date',
-  version: '0.2.2',
+  version: '0.2.3',
   summary: 'A Date class in JavaScript with support for time zones.',
   git: 'https://github.com/aramk/tz-date.git'
 });
