0b98e98276ccd921dd197b0c22b889bd0385685b
Timo Pulkkinen <timo.pulkkinen@cemron.com>
2016-09-20 11:44:39 +0300

    dependency upgrades + include compiled versions in GitHub.

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 // Meteor package definition.
 Package.describe({
   name: 'aramk:tz-date',
-  version: '0.2.3',
+  version: '0.2.4',
   summary: 'A Date class in JavaScript with support for time zones.',
   git: 'https://github.com/aramk/tz-date.git'
 });
@@ -11,7 +11,7 @@ Package.onUse(function (api) {
   api.use([
     'coffeescript',
     'underscore',
-    'risul:moment-timezone@0.3.0_1'
+    'risul:moment-timezone@0.5.4_1'
   ], ['client', 'server']);
   api.export([
     'TimeZoneDate'
